5c46f326ec8bf037780474ba53c54519f56513ae
Al Ameen N <alameen.n@experionglobal.com>
2022-11-26 17:00:06 +0530

    css modified

--- 09-finished/src/components/Layout/MainNavigation.js
+++ 09-finished/src/components/Layout/MainNavigation.js
@@ -5,7 +5,7 @@ import AuthContext from '../../store/auth-context';
 import classes from './MainNavigation.module.css';
 import image from '../images/landingpage.jpg';
 import AboutUs from '../About/AboutUs';
-import picture from '../images/iconimage.png';
+// import picture from '../images/iconimage.png';
 
 
 const MainNavigation = () => {
@@ -21,21 +21,29 @@ const MainNavigation = () => {
   return (
     <header className={classes.header}>
 
-      <div className={classes.headericon}>
+      {/* <div className={classes.headericon}>
         <img src={picture}/>  
-      </div>
+      </div> */}
+      <div className={classes.headericon}></div>
 
       <Link to='/'>
         <div className={classes.logo}>Automotive</div>
       </Link>
       <nav>
         <ul>
+        {/* {!isLoggedIn && (
+
+          <li>
+            <a href='#About'>About</a>
+          </li>
+          )} */}
           {!isLoggedIn && (
 
             <li>
            <Link to='/auth'>Login</Link>
             </li>
           )}
+          
           {isLoggedIn && (
             
             <li >
@@ -57,3 +65,68 @@ const MainNavigation = () => {
 };
 
 export default MainNavigation;
+
+
+
+
+
+
+// import { useContext } from 'react';
+// import { Link } from 'react-router-dom';
+
+// import AuthContext from '../../store/auth-context';
+// import classes from './MainNavigation.module.css';
+// import image from '../images/landingpage.jpg';
+// import AboutUs from '../About/AboutUs';
+// import picture from '../images/iconimage.png';
+
+
+// const MainNavigation = () => {
+//   const authCtx = useContext(AuthContext);
+
+//   const isLoggedIn = authCtx.isLoggedIn;
+
+//   const logoutHandler = () => {
+//     authCtx.logout();
+//     // optional: redirect the user
+//   };
+
+//   return (
+//     <header className={classes.header}>
+
+//       <div className={classes.headericon}>
+//         <img src={picture}/>  
+//       </div>
+
+//       <Link to='/'>
+//         <div className={classes.logo}>Automotive</div>
+//       </Link>
+//       <nav>
+//         <ul>
+//           {!isLoggedIn && (
+
+//             <li>
+//            <Link to='/auth'>Login</Link>
+//             </li>
+//           )}
+//           {isLoggedIn && (
+            
+//             <li >
+//               <strong className='text-light'>Welcome  {authCtx.user.userName}</strong> <Link to='/profile'>Profile</Link>
+//             </li>
+
+//           )}
+//           {isLoggedIn && (
+            
+//               <button onClick={logoutHandler}>Logout</button>
+            
+//           )}
+//         </ul>
+//       </nav>
+      
+//     </header>
+    
+//   );
+// };
+
+// export default MainNavigation;
--- 09-finished/src/components/StartingPage/StartingPageContent.js
+++ 09-finished/src/components/StartingPage/StartingPageContent.js
@@ -1,18 +1,62 @@
+// import classes from './StartingPageContent.module.css';
+
+// const StartingPageContent = () => {
+//   return (
+//     <section className={classes.starting}>
+//       <div className={classes.about}>
+//         <h2>About Us!</h2>
+//         <p>Automotive is a web application for garage management.
+//            Automotive is the best software for managing your garage job card, estimate, invoice,
+//             income, expenses, inventory sell and buys parts and customer interactions</p>
+//       </div>
+      
+//       {/* <h1>Welcome on Board!</h1> */}
+//     </section>
+//   );
+// };
+
+// export default StartingPageContent;
+
 import classes from './StartingPageContent.module.css';
+import { Link } from 'react-router-dom';
+
 
 const StartingPageContent = () => {
   return (
+    <div>
     <section className={classes.starting}>
+      
       <div className={classes.about}>
-        <h2>About Us!</h2>
-        <p>Automotive is a web application for garage management.
-           Automotive is the best software for managing your garage job card, estimate, invoice,
-            income, expenses, inventory sell and buys parts and customer interactions</p>
+        {/* <h2>About Us!</h2> */}
+        <h2>A Complete Garage Management System</h2>
+        <p>We're In The Business Of Helping You Start Your Business</p>
+        
+        {/* <Link to='/auth'>Get Started</Link> */}
+        <Link to='/auth'>
+        <button className={classes.btn}>Get Started</button>
+      </Link>
       </div>
       
       {/* <h1>Welcome on Board!</h1> */}
     </section>
+{/* <section>
+<div className={classes.about}>
+<h2>About Us!</h2>
+<h1 className={classes.heading}>
+            <span>A</span>
+            <span>B</span>
+            <span>O</span>
+            <span>U</span>
+            <span>T</span>
+        </h1>
+<p>Automotive is a web application for garage management.
+   Automotive is the best software for managing your garage job card, estimate, invoice,
+    income, expenses, inventory sell and buys parts and customer interactions</p>
+</div>
+</section> */}
+</div>
   );
 };
 
 export default StartingPageContent;
+
